9d94c68196e8f8feb174e63db884958189e269fd
jimsyyap <jimsyyap@gmail.com>
2020-01-18 10:05:13 +1100

    upload photo

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -6,4 +6,42 @@
     <a class="btn btn-primary" href="/photos/create/{{$album->id}}" role="button">Upload Photo To Album</a>
     <hr>
     
+    @if(count($album->photos) > 0)
+        <?php
+            $colcount = count($album->photos);
+            $i = 1;
+        ?>
+    <div id="photos">
+        <div class="row text-center">
+            @foreach($album->photos as $photo)
+                @if($i == $colcount)
+                    <div class='col-sm-3'>
+                        <a href="/photos/{{$photo->id}}">
+                            <img class="thumbnail" src="/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+                        </a>
+                        <br>
+                        <h4>{{$photo->title}}</h4>
+                    @else
+                        <div class='col-sm-3'>
+                        <a href="/photos/{{$photo->id}}">
+                            <img class="thumbnail" src="/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+                        </a>
+                        <br>
+                        <h4>{{$photo->title}}</h4>
+                        @endif
+                        @if($i % 3 == 0)
+                            </div>
+                            </div>
+                            <div class="text-center">
+                        @else
+                            </div>
+                        @endif
+                        <?php $i++; ?>
+                    @endforeach
+        </div>
+    </div>
+@else
+    <p>No Photos To Display</p>
+@endif
+
 @endsection
