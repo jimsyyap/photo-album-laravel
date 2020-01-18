5a73f617fc9a205036b0624f30e7b80d3bc7114d
jimsyyap <jimsyyap@gmail.com>
2020-01-18 21:14:27 +1100

    done ok

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Photo;
 
 class PhotosController extends Controller
@@ -48,5 +49,15 @@ class PhotosController extends Controller
     public function show($id) {
         $photo = Photo::find($id);
         return view('photos.show')->with('photo', $photo);
+    }
+
+    public function destroy($id){
+        $photo = Photo::find($id);
+
+        if(Storage::delete('public/photos/'.$photo->album_id.'/'.$photo->photo)){
+            $photo->delete();
+
+            return redirect('/')->with('success', 'Photo Deleted');
+      }
     }
 }
--- resources/views/photos/show.blade.php
+++ resources/views/photos/show.blade.php
@@ -5,11 +5,11 @@
     <p>{{$photo->description}}</p>
     <a href="/albums/{{$photo->album_id}}">Back to Gallery</a>
     <hr>
-    <img src="/storage/photos/{{$photo->album_id/{{$photo->photo}}}}" alt="{{$photo->title}}">
+    <img src="/storage/photos/{{ $photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
     <br>
-    {!!Form::open(['action' => '['PhotosController@destroy', $photo->id]', 'method' => 'POST'])!!}
-        {{Form:hidden('_method', 'DELETE')}}
-        {{Form:submit('Delete Photo', ['class' => 'button alert'])}}
+    {!!Form::open(['action' => ['PhotosController@destroy', $photo->id], 'method' => 'POST'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete Photo', ['class' => 'btn btn-danger'])}}
     {!!Form::close()!!}
     <hr>
     <small>Size: {{$photo->size}}</small>
